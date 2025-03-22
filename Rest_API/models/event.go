857f5aa1a5ea8f7fac260eaf2f72a832a9989d40
gggray000 <124888446+gggray000@users.noreply.github.com>
2025-03-22 14:48:54 +0800

    Until Video No.172

---
+++ Rest_API/models/event.go
@@ -0,0 +1,59 @@
+package models
+
+import (
+	"rest-api/database"
+	"time"
+)
+
+type Event struct {
+	ID          int64
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserID      int
+}
+
+func (e Event) Save() error {
+	query := `
+	INSERT INTO events(name, description, location, dateTime, user_id) 
+	VALUES (?,?,?,?,?)
+	`
+
+	// Reusable query preparation
+	statement, err := database.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer statement.Close()
+
+	result, err := statement.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
+}
+
+func GetAllEvents() ([]Event, error) {
+	query := `SELECT * FROM events`
+	// Querry() only fetch, whereas Exec() can change data
+	rows, err := database.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	var events = []Event{}
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+
+	return events, nil
+}
