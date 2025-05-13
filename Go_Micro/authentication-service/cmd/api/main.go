356ecbf1bb2694bcdff735c8edd3961e632c542f
gggray000 <124888446+gggray000@users.noreply.github.com>
2025-05-14 00:04:49 +0200

    Finished the course.

--- Go_Micro/authentication-service/cmd/api/main.go
+++ Go_Micro/authentication-service/cmd/api/main.go
@@ -18,8 +18,10 @@ const webPort = "80"
 var counts int64
 
 type Config struct {
-	DB     *sql.DB
-	Models data.Models
+	//DB     *sql.DB
+	//Models data.Models
+	Repo   data.Repository
+	Client *http.Client
 }
 
 func main() {
@@ -32,8 +34,9 @@ func main() {
 	}
 	// Set up config
 	app := Config{
-		DB:     conn,
-		Models: data.New(conn),
+		//DB:     conn,
+		//Models: data.New(conn),
+		Client: &http.Client{},
 	}
 
 	srv := &http.Server{
@@ -83,4 +86,10 @@ func connectToDB() *sql.DB {
 		time.Sleep(2 * time.Second)
 		continue
 	}
+}
+
+// Added for test
+func (app *Config) setupRepo(conn *sql.DB) {
+	db := data.NewPostgresRepository(conn)
+	app.Repo = db
 }
